3ad94dee52bba048ad1103e46ef9b84cabafb649
Atsura <94127380+Antwannnn@users.noreply.github.com>
2022-10-29 10:18:05 +0200

    Add files via upload

--- src/SAEv2.java
+++ src/SAEv2.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
 
 public class SAEv2 {
-
+    // déclaration du scanner pour toute la classe
     private static Scanner scanner = new Scanner(System.in);
 
+    /** Rôle : première fonction a s'écuter et on y retrouve la plupart des fonctions
+     * @param args IN : convention
+     */
     public static void main(String[] args){
 
-        int nbCandidats, nbDisqualifiés, nbPortes, z;
+        int nbCandidats, nbCandidatsRestants, nbPortes;
         double[] tabTempsManche1;
         double[] tabTempsManche2;
         double[] tabClassementFinal = new double[0];
 
-        int[] numeroDeBrassard = {0};
+        int[] numeroDeBrassard;
 
         int nbPortesRateesManche1, nbPortesRateesManche2, nbPortesToucheesManche1, nbPortesToucheesManche2;
 
@@ -23,13 +26,12 @@ public class SAEv2 {
         tabTempsManche1 = new double[nbCandidats];
         tabTempsManche2 = new double[nbCandidats];
         tabClassementFinal = new double[nbCandidats];
-        z = 0;
+        numeroDeBrassard = new int[nbCandidats];
+        nbCandidatsRestants = 0;
+
         nbPortes = saisirNbPortes();
 
-        int delegate = nbCandidats;
-
-        for(int i = 0; i < delegate; i++){
-            numeroDeBrassard = new int[nbCandidats + 1];
+        for(int i = 0; i < nbCandidats; i++){
 
             System.out.println("Informations pour le " + (i+1) + " ème participant");
             System.out.println("-----------------------------------------------");
@@ -37,94 +39,156 @@ public class SAEv2 {
             boolean disqualifié = saisirDisqualification();
 
             if(!disqualifié){
-                saisirTabTemps(tabTempsManche1, nbCandidats, "Veuillez saisir le temps pour le " + (i+1) + "ème participant pour la manche 1: ", (i));
+                saisirTabTemps(tabTempsManche1, "Veuillez saisir le temps pour le " + (i+1) + "ème participant pour la manche 1: ", (i));
                 nbPortesRateesManche1 = saisirInt(nbPortes, "Veuillez saisir le nombre de portes ratées à la manche 1 pour le " + (i+1) + "ème participant : ");
                 nbPortesToucheesManche1 = saisirInt(nbPortes, "Veuillez saisir le nombre de portes touchées à la manche 1 pour le " + (i+1) + "ème participant : ");
+                while (nbPortesToucheesManche1 + nbPortesRateesManche1 > nbPortes) {
+                    nbPortesToucheesManche1 = saisirInt(nbPortes, "Veuillez saisir le nombre de portes touchées à la manche 1 pour le " + (i+1) + "ème participant : ");
+                }
                 tempsCompensesManche1 = tempsCompensé(nbPortesToucheesManche1, nbPortesRateesManche1, tabTempsManche1[i]);
 
-                saisirTabTemps(tabTempsManche2, nbCandidats, "Veuillez saisir le temps pour le " + (i+1) + "ème participant pour la manche 2: ", (i));
+                saisirTabTemps(tabTempsManche2, "Veuillez saisir le temps pour le " + (i+1) + "ème participant pour la manche 2: ", (i));
                 nbPortesRateesManche2 = saisirInt(nbPortes, "Veuillez saisir le nombre de portes ratées à la manche 2 pour le " + (i+1) + "ème participant : ");
                 nbPortesToucheesManche2 = saisirInt(nbPortes, "Veuillez saisir le nombre de portes touchées à la manche 2 pour le " + (i+1) + "ème participant : ");
+                while (nbPortesToucheesManche2 + nbPortesRateesManche2 > nbPortes) {
+                    nbPortesToucheesManche2 = saisirInt(nbPortes, "Veuillez saisir le nombre de portes touchées à la manche 1 pour le " + (i+1) + "ème participant : ");
+                }
                 tempsCompensesManche2 = tempsCompensé(nbPortesToucheesManche2, nbPortesRateesManche2, tabTempsManche2[i]);
 
                 numeroDeBrassard[i] = i + 1;
 
                 moyenneTempsCompenses = (tempsCompensesManche1 + tempsCompensesManche2) / 2;
-                tabClassementFinal[z] = moyenneTempsCompenses;
+                tabClassementFinal[nbCandidatsRestants] = moyenneTempsCompenses;
 
                 System.out.println(afficherTempsConverti(tabClassementFinal[i], i));
-                z++;
+                nbCandidatsRestants++;
 
             }
 
 
         }
-        System.out.println(nbCandidats);
-        for (int xyz = 0; xyz < nbCandidats; xyz++) {
-            System.out.println(tabClassementFinal[xyz]);
-            System.out.println(numeroDeBrassard[xyz]);
-        }
-        //podium(nbCandidats, tabClassementFinal, numeroDeBrassard);
+        podium(nbCandidatsRestants, tabClassementFinal, numeroDeBrassard);
 
 
     }
+    /** Rôle : permet de saisir un entier qui est inférieur à la borne et supérieur à 0
+     * @param pfBorne IN : borne que la valeur saisi doit dépasser
+     * @param pfMessage IN : message à afficher pour faire saisir la valeur
+     * @return la valeur saisie
+     */
     public static int saisirInt(int pfBorne, String pfMessage){
+        // on met value à -1 pour pouvoir entrer dans la boucle while
         int value = -1;
+
+        // tant que la valeur saisie est supérieur à la borne (entrée en paramètre) et inférieur à 0 
+        // alors on affiche le message et on demande de saisir une valeur
         while(value > pfBorne || value < 0){
             System.out.print(pfMessage);
             value = scanner.nextInt();
         }
+
+        // on renvoi la valeur
         return value;
     }
 
-    public static void saisirTabTemps(double[] pfTab, int pfTailleTab, String pfMessage, int pfIndice){
+    /** Rôle : permet de saisir un temps de course qui doit donc être supérieur à 0
+     * @param pfTab IN : permet de remplir un tableau de temps (manche 1 et manche 2)
+     * @param pfMessage IN : message à afficher
+     * @param pfIndice IN : indice pour modifier le tableau
+     */
+    public static void saisirTabTemps(double[] pfTab, String pfMessage, int pfIndice){
+        // on initialise la valeur à modifier du tableau à 0 pour rentrer dans la boucle while
         pfTab[pfIndice] = 0;
-        System.out.println();
+
+        // tant que la valeur qu'on fait saisir est inférieur ou égal à 0 (car un score ne peut pas être négatif ou nul)
+        // alors on affiche le message et on lui fait saisir une valeur
         while(pfTab[pfIndice] <= 0){
             System.out.print(pfMessage);
             pfTab[pfIndice] = scanner.nextDouble();
         }
     }
-    public static int saisirNbPortes (
 
-    ) {
+    /** Rôle : permet de saisir le nombre de portes qui doit être compris entre 18 et 22
+     * @return le nombre de portes
+     */
+    public static int saisirNbPortes () {
+        // initialisation
         int NbPortes;
+
+        // on affiche le message pour faire comprendre à l'utilisateur de rentrer le nombre de portes
         System.out.print("Saisissez le nombre de portes de la course, il doit être compris entre 18 et 22 : ");
+        // on fait rentrer à l'utilisateur le nombre de porte
         NbPortes = scanner.nextInt();
+
+        // tant que le nombre de portes est inférieur à 18 ou supérieur à 22 
+        // alors on lui affiche de nouveau le message et on lui fait re saisir le nombre de portes
         while (NbPortes < 18 || NbPortes > 22) {
             System.out.print("Veuillez saisir un nombre entre 18 et 22 compris: ");
             NbPortes = scanner.nextInt();
         }
+
+        // on renvoi le nombre de portes
         return NbPortes;
     }
 
+    /** Rôle : permet de saisir le nombre de canditat qui doit être compris entre 1 et 49
+     * @return le nombre de candidats
+     */
     public static int saisirNbCandidats (){
+        // initialisation
         int NbCandidats;
+
+        // on affiche le message pour faire comprendre à l'utilisateur de rentrer le nombre de candidats
         System.out.print("Saisissez le nombre de candidats à la course, il doit être compris entre 1 et 49 : ");
+        // on fait rentrer à l'utilisateur le nombre de candidats
         NbCandidats = scanner.nextInt();
+
+        // tant que le nombre de candidat est inférieur ou égal à 0 ou 50 
+        // alors on lui affiche de nouveau le message et on lui fait re saisir le nombre de candidat
         while(NbCandidats<1 || NbCandidats >= 50){
             System.out.print("Veuillez saisir un nombre entre 1 et 49 compris: ");
             NbCandidats = scanner.nextInt();
         }
+
+        // on renvoi le nombre de candidats
         return NbCandidats;
     }
 
+    /** Rôle : permet de voir si un candidat est disqualifié ou non
+     * @return true si le candidat est disqualifié et false sinon
+     */
     public static boolean saisirDisqualification() {
+        // on affiche ce message pour faire comprendre à l'utilisateur de rentrer "true" ou "false" pour savoir si un candidat est disqualifé
         System.out.print("Ce joueur est-il disqualifié ? true ou false: ");
         boolean value;
         value = scanner.nextBoolean();
 
+        // on renvoi la valeur
         return value;
     }
 
+    /** Rôle : permet de calculer le score d'un candidat en rajoutant les pénalités 
+     * @param portesratées IN : nombre de portes ratées
+     * @param portestouchées IN : nombre de portes touchées
+     * @param tempsManche IN : le score actuel du candidat (avec de rajoter les pénalités)
+     * @return le temps compensé (le temps + les pénalités)
+     */
     public static double tempsCompensé(int portesratées, int portestouchées, double tempsManche){
+        // initialisation
         double tempsFinal, tempsPenalite;
+
+        // calcul des pénalités pour chaque portes ratées
         int pénalitéPortesRatées = 50*1000*portesratées;
+        // calcul des pénalités pour chaque portes touchées
         int pénalitéPortesTouchées = 2*1000*portestouchées;
 
+        // somme des pénalités des portes touchées et ratées
         tempsPenalite = pénalitéPortesTouchées + pénalitéPortesRatées;
+
+        // somme du score final (score de départ + pénalités)
         tempsFinal = tempsManche + tempsPenalite;
 
+        // on renvoi le score final
         return tempsFinal;
     }
 
@@ -213,11 +277,6 @@ public class SAEv2 {
         deuxieme = 0;
         troisieme = 0;
 
-        System.out.println("Nombre de candidats : " + pfNbCompetiteur);
-        for (i = 0; i < pfNbCompetiteur; i++) {
-            System.out.println(i+1 + " Score : " + pfScore[i]);
-            System.out.println(i+1 + " Brassard : " + pfBrassard[i]);
-        }
         // traitement
 
         // si il y a au moins 1 compétiteur
@@ -262,62 +321,69 @@ public class SAEv2 {
         // affichage
         // si il y a éxactement 1 premier
         if (nbPremier == 1) {
-            System.out.println("Nous avons en première position avec un temps de " + premier + "sec le brassard n°" + pfBrassard[indicePremier[0]]);
+            System.out.println("Nous avons en première position avec un temps de " + premier + " millisecondes le brassard n°" + pfBrassard[indicePremier[0]]);
             System.out.println(afficherTempsConverti(premier, 1));
         }
         // si il y a plusieurs premiers
         else if (nbPremier >= 2) {
-            System.out.println("Nous avons " + nbPremier + " ex æquo  pour la première position avec un temps de " + premier + "sec qui sont les brassard n° ");
+            System.out.println("Nous avons " + nbPremier + " ex æquo  pour la première position avec un temps de " + premier + " millisecondes qui sont les brassard n° ");
             for (i = 0; i < nbPremier; i++) {
-                System.out.print(pfBrassard[indicePremier[i]]);
+                System.out.println(pfBrassard[indicePremier[i]]);
             }
             System.out.println(afficherTempsConverti(premier, 1));
         }
 
         // si il y a éxactement 1 deuxième
         if (nbDeuxieme == 1) {
-            System.out.println("Nous avons en deuxième position avec un temps de " + deuxieme + "sec le brassard n°" + pfBrassard[indiceDeuxieme[0]]);
+            System.out.println("Nous avons en deuxième position avec un temps de " + deuxieme + " millisecondes le brassard n°" + pfBrassard[indiceDeuxieme[0]]);
             System.out.println(afficherTempsConverti(deuxieme, 2));
         }
         // si il y a plusieurs deuxième
         else if (nbDeuxieme >= 2) {
-            System.out.println("Nous avons " + nbDeuxieme + " ex æquo  pour la deuxième position avec un temps de " + deuxieme + "sec qui les brassard n°");
+            System.out.println("Nous avons " + nbDeuxieme + " ex æquo  pour la deuxième position avec un temps de " + deuxieme + " millisecondes qui les brassard n°");
             for (i = 0; i < nbDeuxieme; i++) {
-                System.out.print(pfBrassard[indiceDeuxieme[i]]);
+                System.out.println(pfBrassard[indiceDeuxieme[i]]);
             }
             System.out.println(afficherTempsConverti(deuxieme, 2));
         }
 
         // si il y a éxactement 1 troisième
         if (nbTroisieme == 1){
-            System.out.println("Nous avons en troisième position un temps de " + troisieme + "sec le brassard n°" + pfBrassard[indiceTroisieme[0]]);
+            System.out.println("Nous avons en troisième position un temps de " + troisieme + " millisecondes le brassard n°" + pfBrassard[indiceTroisieme[0]]);
             System.out.println(afficherTempsConverti(troisieme, 3));
         }
         // si il y a plusieurs troisième
         else if (nbTroisieme >= 2) {
-            System.out.println("Nous avons " + nbTroisieme + " ex æquo  pour la troisième position avec un temps de " + troisieme + "sec qui sont les brassard n°");
+            System.out.println("Nous avons " + nbTroisieme + " ex æquo  pour la troisième position avec un temps de " + troisieme + " millisecondes qui sont les brassard n°");
             for (i = 0; i < nbTroisieme; i++) {
-                System.out.print(pfBrassard[indiceTroisieme[i]]);
+                System.out.println(pfBrassard[indiceTroisieme[i]]);
             }
             System.out.println(afficherTempsConverti(troisieme, 3));
         }
     }
 
+    /** Rôle : permet d'afficher le temps converti des candidats qui sont sur le podium
+     * @param pfTemps IN : score du candidat
+     * @param indice IN : indice du joueur (place sur le podium)
+     * @return le temps converti
+     */
     public static String afficherTempsConverti(double pfTemps, int indice){
+        // initialisation du message, des minutes et des secondes
         String message = "";
         int min;
         double secondes;
 
-        min =0;
-            System.out.println("Le " + indice +" ème joueur a réalisé un temps de " +pfTemps+ " millisecondes, c'est-à-dire un temps de "+pfTemps/1000+" secondes.");
-            secondes = pfTemps/1000;
-            while(secondes > 60){
-                min += 1;
-                secondes -= 60;
 
-            }
-            System.out.println("Cela correspond à un temps de "+min+" minutes et "+secondes+ " secondes.");
-            min =0;
+        min =0;
+        System.out.println("Le " + indice +" ème joueur a réalisé un temps de " +pfTemps+ " millisecondes, c'est-à-dire un temps de "+pfTemps/1000+" secondes.");
+        secondes = pfTemps/1000;
+        while(secondes > 60){
+            min += 1;
+            secondes -= 60;
+
+        }
+        message = "Cela correspond à un temps de "+min+" minutes et "+secondes+ " secondes.";
+        min =0;
         return message;
     }
 
